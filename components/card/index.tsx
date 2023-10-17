0c3891d137137e173031816a386d5f2267c5f011
mhbaando <maxamuudyare21@gmail.com>
2023-10-17 12:08:00 +0300

    implemented card fro my components

---
+++ components/card/index.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+
+const Card = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+  className?: string;
+}) => {
+  return (
+    <div className="rounded-md bg-gradient-to-br from-white/70 via-black/95 to-transparent p-[0.5px] relative">
+      <div className="rounded-md bg-zinc-500 backdrop-blur-2xl drop-shadow-lg">
+        <div className="bg-gradient-to-r from-black via-black to-black/95 p-4 rounded-md">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;

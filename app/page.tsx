ff32ae4812afb10220becd31b77bf1be5ab77fc3
mhbaando <maxamuudyare21@gmail.com>
2023-10-28 22:26:28 +0300

    all componens are included

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Hero from "@/components/hero";
 import About from "@/components/about";
 import Wrapper from "@/components/wrapper";
 import Experience from "@/components/experience";
+import CallToAction from "@/components/cta";
+import Services from "@/components/services";
+import Tools from "@/components/tools";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
@@ -10,7 +14,11 @@ export default function Home() {
         <Hero />
         <About />
         <Experience />
+        <CallToAction />
+        <Services />
+        <Tools />
       </Wrapper>
+      <Footer />
     </main>
   );
 }
